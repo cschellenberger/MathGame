a54cee6c9e67d9bf01e1d17456637ad2c0f1218d
Cody Schellenberger <CODYS@nait.ca>
2023-08-12 12:57:00 -0400

    Enter new methods for modularization

--- MathGame_VS/ConsoleApp2/MathGame.cs
+++ MathGame_VS/ConsoleApp2/MathGame.cs
@@ -1,14 +1,54 @@
-﻿using System;
+﻿/*
+    Description: This program is a math game that asks the user to add two random numbers together.
+    The user can choose to have the tutor help them by showing them the sum of the two numbers.
+    The user can also choose to play again or quit the game.
+
+    Inputs: The user's name, the user's guess, and the user's choice to play again or not.
+
+    Outputs: The user's name, the user's score, and the correct answer to the math problem.
+
+    Variables: firstName, yesNo, tryAgain, tutor, MIN_NUMBER, MAX_NUMBER, correctAnswers, 
+               wrongAnswers, firstNumber, secondNumber, sum, userGuess, successful, frequency, 
+               i, countSum, count, welcomeMessage
+
+    Constants: MIN_NUMBER, MAX_NUMBER
+
+    Summarized Algorithm: 
+        1. Ask the user for their name.
+        2. Welcome the user to the game.
+        3. Ask the user if they want to play the game.
+        4. Generate two random numbers between 0 and 25.
+        5. Add the two random numbers together.
+        6. Ask the user for their guess.
+        7. If the user's guess is correct, congratulate them and add 1 to their score.
+        8. If the user's guess is incorrect, ask them if they want to try again.
+        9. If the user wants to try again, ask them if they want help from the tutor.
+        10. If the user wants help from the tutor, show them the sum of the two random numbers.
+        11. If the user does not want help from the tutor, ask them for their guess again.
+        12. If the user does not want to try again, ask them if they want to play again.
+        13. If the user wants to play again, generate two new random numbers.
+        14. If the user does not want to play again, thank them for playing the game and show them their score.
+        15. End the program.
+*/
+
+using System;
+
 string welcomeMessage = "Welcome to the Math Game!";
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine(welcomeMessage);
 Console.ForegroundColor = ConsoleColor.Red;
-for (int i = 0; i < 25; i++)
+
+// A for loop that prints out a line of dashes the same length as the welcome message.
+for (int i = 0; i < welcomeMessage.Length; i++)
 {
     Console.Write("-");
 }
+
+// Reset the console color to the default color.
 Console.ResetColor();
 
+// Ask the user for their name.
+// The ? after the data type means that the variable can be null.
 string? firstName;
 Console.WriteLine();
 Console.Write("What is your name? ");
@@ -74,10 +114,13 @@ while (yesNo == 'y')
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"Good job! That is correct. {firstNumber} + {secondNumber} = {sum}");
-            for (double i = 0, frequency = 220; i < 3; i++, frequency *= 1.5)
-            {
-                Console.Beep((int)frequency, 250);
-            }
+            
+            // This method is not supported on Linux.
+            PlaySuccessTone();
+            
+            // Play a system alert tone.
+            // Console.Write("\a");
+            
             Console.ResetColor();
             tryAgain = 'n';
             tutor = 'n';
@@ -87,10 +130,13 @@ while (yesNo == 'y')
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Write($"That is not correct yet. Do you want to try again? (y/n): ");
-            for (double i = 0, frequency = 600; i < 3; i++, frequency *= 0.9)
-            {
-                Console.Beep((int)frequency, 250);
-            }
+
+            // This method is not supported on Linux.
+            PlayErrorTone();
+
+            // Play a system alert tone.
+            Console.Write("\a");
+
             Console.ResetColor();
             tryAgain = Console.ReadKey().KeyChar;
             Console.WriteLine();
@@ -126,3 +172,37 @@ Console.ResetColor();
 Console.WriteLine();
 Console.Write("Press any key to close the console: ");
 Console.ReadKey();
+
+void PlayErrorTone()
+{
+    if (OperatingSystem.IsWindows())
+    {
+        for (double i = 0, frequency = 600; i < 3; i++, frequency *= 0.9)
+        {
+            // This method is not supported on Linux.
+            Console.Beep((int)frequency, 250);
+        }
+    }
+    
+    else
+    {
+        Console.Write("\a");
+    }
+}
+
+void PlaySuccessTone()
+{
+    if (OperatingSystem.IsWindows())
+    {
+        for (double i = 0, frequency = 220; i < 3; i++, frequency *= 1.5)
+        {
+            // This method is not supported on Linux.
+            Console.Beep((int)frequency, 250);
+        }
+    }
+
+    else
+    {
+        Console.Write("\a");
+    }
+}
